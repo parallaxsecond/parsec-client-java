881f64b30dfc020f564f4b1caaf11b7dd25e899c
Sam Spycher <samspycher@users.noreply.github.com>
2021-11-24 01:15:55 +0100

    test log message with name parameter

--- parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecSignature.java
+++ parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecSignature.java
@@ -69,7 +69,7 @@ public final class ParsecSignature extends SignatureSpi {
           parsecClientAccessor
               .get()
               .psaSignHash(keyName, digest, signatureInfo.getParsecAlgorithm());
-      log.info("Signed with algorithm {)", signatureInfo.getAlgorithmName());
+      log.info("Signed with algorithm {)", signatureInfo.name());
 
       return r.getSignature();
     } catch (ServiceException | ClientException e) {
