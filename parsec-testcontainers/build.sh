e5a26e7c35a520fd89bc3dad8b38df5d2c886baa
Sam Spycher <samspycher@users.noreply.github.com>
2025-03-26 17:29:18 +0100

    fix: only move parsec_docker_cache_new if it exists, improve logging
    Signed-off-by: Sam Spycher <samspycher@users.noreply.github.com>

--- parsec-testcontainers/build.sh
+++ parsec-testcontainers/build.sh
@@ -2,9 +2,12 @@
 docker_cache=parsec_docker_cache
 
 CACHE_CONFIG=""
-if (docker buildx inspect |grep "Driver: docker-container"); then
+if (docker buildx inspect | grep "Driver: docker-container"); then
   CACHE_CONFIG=" --set *.cache-from=type=local,src=${docker_cache} --set *.cache-to=mode=max,type=local,dest=${docker_cache}_new"
 fi
+
+echo "docker buildx command:"
+echo "docker buildx bake ${CACHE_CONFIG} --progress plain --load"
 
 # shellcheck disable=SC2086
 docker buildx bake ${CACHE_CONFIG} \
@@ -12,4 +15,6 @@ docker buildx bake ${CACHE_CONFIG} \
   --load
 
 rm -rf ${docker_cache} || true
-mv ${docker_cache}_new ${docker_cache} || true
+if [ -d "${docker_cache}_new" ]; then
+  mv ${docker_cache}_new ${docker_cache} || true
+fi
