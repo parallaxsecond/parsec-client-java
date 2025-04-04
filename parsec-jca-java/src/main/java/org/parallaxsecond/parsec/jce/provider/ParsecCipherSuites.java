0d6bf8feb7f258617bfcabf936f5e84537530025
Sam Spycher <samspycher@users.noreply.github.com>
2025-04-04 17:03:22 +0200

    fix: add key length to RSA_WITH_PKCS1
    Signed-off-by: Sam Spycher <samspycher@users.noreply.github.com>

--- parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecCipherSuites.java
+++ parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecCipherSuites.java
@@ -1,53 +1,44 @@
 package org.parallaxsecond.parsec.jce.provider;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.parallaxsecond.parsec.protobuf.psa_algorithm.PsaAlgorithm;
 import org.parallaxsecond.parsec.protobuf.psa_key_attributes.PsaKeyAttributes;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ParsecCipherSuites {
 
-    RSA_WITH_PKCS1(
-            PsaKeyAttributes.KeyAttributes.newBuilder()
-                    .setKeyPolicy(
-                            PsaKeyAttributes.KeyPolicy.newBuilder()
+        RSA_WITH_PKCS1(PsaKeyAttributes.KeyAttributes.newBuilder()
+                        .setKeyPolicy(PsaKeyAttributes.KeyPolicy.newBuilder().setKeyAlgorithm(
+                                        PsaAlgorithm.Algorithm.newBuilder().setAsymmetricSignature(
+                                                        PsaAlgorithm.Algorithm.AsymmetricSignature
+                                                                        .newBuilder()
+                                                                        .setRsaPkcs1V15Sign(
+                                                                                        PsaAlgorithm.Algorithm.AsymmetricSignature.RsaPkcs1v15Sign
+                                                                                                        .newBuilder()
+                                                                                                        .setHashAlg(PsaAlgorithm.Algorithm.AsymmetricSignature.SignHash
+                                                                                                                        .newBuilder()
+                                                                                                                        .setAny(PsaAlgorithm.Algorithm.AsymmetricSignature.SignHash.Any
+                                                                                                                                        .newBuilder()
+                                                                                                                                        .build())
+                                                                                                                        .build())
+                                                                                                        .build())
+                                                                        .build())
+                                                        .build())
+                                        .setKeyUsageFlags(PsaKeyAttributes.UsageFlags.newBuilder()
+                                                        .setDecrypt(true).setEncrypt(true)
+                                                        .setSignMessage(true).setVerifyMessage(true)
+                                                        .setVerifyHash(true).setSignHash(true)
+                                                        .build())
+                                        .build())
+                        .setKeyType(PsaKeyAttributes.KeyType.newBuilder()
+                                        .setRsaKeyPair(PsaKeyAttributes.KeyType.RsaKeyPair
+                                                        .newBuilder().build())
+                                        .build())
+                        .setKeyBits(4096).build());
 
-                                    .setKeyAlgorithm(
-
-                                            PsaAlgorithm.Algorithm.newBuilder()
-
-                                                    .setAsymmetricSignature(
-                                                            PsaAlgorithm.Algorithm
-                                                                    .AsymmetricSignature
-                                                                    .newBuilder()
-                                                                    .setRsaPkcs1V15Sign(
-                                                                            PsaAlgorithm.Algorithm.AsymmetricSignature.RsaPkcs1v15Sign.newBuilder()
-                                                                                    .setHashAlg(PsaAlgorithm.Algorithm.AsymmetricSignature.SignHash.newBuilder()
-                                                                                            .setAny(PsaAlgorithm.Algorithm.AsymmetricSignature.SignHash.Any.newBuilder().build())
-                                                                                            .build())
-                                                                                    .build()
-                                                                    )
-                                                                    .build())
-                                                    .build())
-                                    .setKeyUsageFlags(
-                                            PsaKeyAttributes.UsageFlags.newBuilder()
-                                                    .setDecrypt(true)
-                                                    .setEncrypt(true)
-                                                    .setSignMessage(true)
-                                                    .setVerifyMessage(true)
-                                                    .setVerifyHash(true)
-                                                    .setSignHash(true)
-                                                    .build())
-                                    .build())
-                    .setKeyType(
-                            PsaKeyAttributes.KeyType.newBuilder()
-                                    .setRsaKeyPair(
-                                            PsaKeyAttributes.KeyType.RsaKeyPair.newBuilder()
-                                                    .build())
-                                    .build())
-                    .build());
-    @Getter
-    private final PsaKeyAttributes.KeyAttributes keyAttributes;
+        @Getter
+        private final PsaKeyAttributes.KeyAttributes keyAttributes;
 
 }
