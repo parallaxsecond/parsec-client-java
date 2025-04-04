49fc1a093040ba00d9d578a9543fb2e96918affc
Sam Spycher <samspycher@users.noreply.github.com>
2025-04-04 17:35:43 +0200

    formattting
    Signed-off-by: Sam Spycher <samspycher@users.noreply.github.com>

--- parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecProvider.java
+++ parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecProvider.java
@@ -1,11 +1,5 @@
 package org.parallaxsecond.parsec.jce.provider;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-import org.parallaxsecond.parsec.client.core.BasicClient;
-import org.parallaxsecond.parsec.client.core.ipc_handler.IpcHandler;
-
 import java.net.URI;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
@@ -13,113 +7,97 @@ import java.security.ProviderException;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+import org.parallaxsecond.parsec.client.core.BasicClient;
+import org.parallaxsecond.parsec.client.core.ipc_handler.IpcHandler;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 /** Parsec JCA Security Provider */
 @Slf4j
 public final class ParsecProvider extends Provider {
-    public static final String MESSAGE_DIGEST = "MessageDigest";
-    public static final String SECURE_RANDOM = "SecureRandom";
-    public static final String PROVIDER_NAME = "PARSEC";
-    private static final double VERSION = 648000 / Math.PI;
-    @Getter private final ParsecClientAccessor parsecClientAccessor;
+        public static final String MESSAGE_DIGEST = "MessageDigest";
+        public static final String SECURE_RANDOM = "SecureRandom";
+        public static final String PROVIDER_NAME = "PARSEC";
+        private static final double VERSION = 648000 / Math.PI;
+        @Getter
+        private final ParsecClientAccessor parsecClientAccessor;
 
-    /**
-     * Constructs a provider with .
-     *
-     * @param socketUri URI of the domain socket the parsec daemon listens on.
-     */
-    @Builder
-    public ParsecProvider(URI socketUri, String parsecAppName) {
-        super(
-                PROVIDER_NAME,
-                VERSION,
-                String.format("%s provider, version %s.", PROVIDER_NAME, VERSION));
-        // create a new client each time for now
+        /**
+         * Constructs a provider with .
+         *
+         * @param socketUri URI of the domain socket the parsec daemon listens on.
+         */
+        @Builder
+        public ParsecProvider(URI socketUri, String parsecAppName) {
+                super(PROVIDER_NAME, VERSION,
+                                String.format("%s provider, version %s.", PROVIDER_NAME, VERSION));
+                // create a new client each time for now
 
-        if (parsecAppName == null) {
-            parsecAppName = "parsec-jca-provider";
-        }
-        final String parsecAppName_ = parsecAppName;
-
-        this.parsecClientAccessor =
-                () ->
-                        BasicClient.client(parsecAppName_, IpcHandler.connectFromUrl(socketUri));
-
-        ps(
-                MESSAGE_DIGEST,
-                "None",
-                NoneMessageDigest.class.getCanonicalName(),
-                NoneMessageDigest::new);
-        ps(
-                SECURE_RANDOM,
-                "NativePRNG",
-                SecureRandomParsec.class.getCanonicalName(),
-                SecureRandomParsec::new);
-        ps(
-                SECURE_RANDOM,
-                "NativePRNGBlocking",
-                SecureRandomParsec.class.getCanonicalName(),
-                SecureRandomParsec::new);
-        ps(
-                SECURE_RANDOM,
-                "NativePRNGNonBlocking",
-                SecureRandomParsec.class.getCanonicalName(),
-                SecureRandomParsec::new);
-
-        ps(
-                "KeyManagerFactory",
-                "X509",
-                KeyManagerFactoryImpl.class.getCanonicalName(),
-                KeyManagerFactoryImpl::new);
-
-        Stream.of(ParsecRsaSignature.values()).forEach(this::signature);
-    }
-
-    private void signature(ParsecSignatureInfo parsecSignatureInfo) {
-        ps("Signature",
-                parsecSignatureInfo.getAlgorithmName(),
-                ParsecSignature.class.getCanonicalName(),
-                parsecSignatureInfo::create);
-    }
-    private void ps(
-            String type,
-            String algorithm,
-            String className,
-            Function<ParsecClientAccessor, Object> parsecClientFactory) {
-        putService(
-                new ParsecProviderService(this, type, algorithm, className, parsecClientFactory));
-    }
-
-    private static final class ParsecProviderService extends Provider.Service {
-        private final Function<ParsecClientAccessor, Object> objectFactory;
-
-        ParsecProviderService(
-                Provider p,
-                String type,
-                String algo,
-                String className,
-                Function<ParsecClientAccessor, Object> objectFactory) {
-            super(p, type, algo, className, null, null);
-            this.objectFactory = objectFactory;
-        }
-
-        @Override
-        public Object newInstance(Object ctrParamObj) throws NoSuchAlgorithmException {
-            try {
-                if (getProvider() instanceof ParsecProvider) {
-                    return this.objectFactory.apply(
-                            ((ParsecProvider) getProvider()).getParsecClientAccessor());
+                if (parsecAppName == null) {
+                        parsecAppName = "parsec-jca-provider";
                 }
-            } catch (Exception e) {
-                throw new ProviderException(
-                        String.format(
-                                "Error constructing object for algorithm: %s, type: %s, provider %s",
-                                getAlgorithm(), getType(), getProvider()),
-                        e);
-            }
-            throw new ProviderException(
-                    String.format(
-                            "No implementation for algorithm: %s, type: %s, provider %s",
-                            getAlgorithm(), getType(), getProvider()));
+                final String parsecAppName_ = parsecAppName;
+
+                this.parsecClientAccessor = () -> BasicClient.client(parsecAppName_,
+                                IpcHandler.connectFromUrl(socketUri));
+
+                ps(MESSAGE_DIGEST, "None", NoneMessageDigest.class.getCanonicalName(),
+                                NoneMessageDigest::new);
+
+                ps(SECURE_RANDOM, "NativePRNG", SecureRandomParsec.class.getCanonicalName(),
+                                SecureRandomParsec::new);
+                ps(SECURE_RANDOM, "NativePRNGBlocking", SecureRandomParsec.class.getCanonicalName(),
+                                SecureRandomParsec::new);
+                ps(SECURE_RANDOM, "NativePRNGNonBlocking",
+                                SecureRandomParsec.class.getCanonicalName(),
+                                SecureRandomParsec::new);
+
+                ps("KeyManagerFactory", "X509", KeyManagerFactoryImpl.class.getCanonicalName(),
+                                KeyManagerFactoryImpl::new);
+
+                Stream.of(ParsecRsaSignature.values()).forEach(this::signature);
+
         }
-    }
+
+        private void signature(ParsecSignatureInfo parsecSignatureInfo) {
+                ps("Signature", parsecSignatureInfo.getAlgorithmName(),
+                                ParsecSignature.class.getCanonicalName(),
+                                parsecSignatureInfo::create);
+        }
+
+        private void ps(String type, String algorithm, String className,
+                        Function<ParsecClientAccessor, Object> parsecClientFactory) {
+                putService(new ParsecProviderService(this, type, algorithm, className,
+                                parsecClientFactory));
+        }
+
+        private static final class ParsecProviderService extends Provider.Service {
+                private final Function<ParsecClientAccessor, Object> objectFactory;
+
+                ParsecProviderService(Provider p, String type, String algo, String className,
+                                Function<ParsecClientAccessor, Object> objectFactory) {
+                        super(p, type, algo, className, null, null);
+                        this.objectFactory = objectFactory;
+                }
+
+                @Override
+                public Object newInstance(Object ctrParamObj) throws NoSuchAlgorithmException {
+                        try {
+                                if (getProvider() instanceof ParsecProvider) {
+                                        return this.objectFactory
+                                                        .apply(((ParsecProvider) getProvider())
+                                                                        .getParsecClientAccessor());
+                                }
+                        } catch (Exception e) {
+                                throw new ProviderException(String.format(
+                                                "Error constructing object for algorithm: %s, type: %s, provider %s",
+                                                getAlgorithm(), getType(), getProvider()), e);
+                        }
+                        throw new ProviderException(String.format(
+                                        "No implementation for algorithm: %s, type: %s, provider %s",
+                                        getAlgorithm(), getType(), getProvider()));
+                }
+        }
 }
--- parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecRsaSignature.java
+++ parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecRsaSignature.java
@@ -1,52 +1,51 @@
 package org.parallaxsecond.parsec.jce.provider;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import java.security.MessageDigest;
+
 import org.parallaxsecond.parsec.protobuf.psa_algorithm.PsaAlgorithm;
 import org.parallaxsecond.parsec.protobuf.psa_algorithm.PsaAlgorithm.Algorithm.AsymmetricSignature;
 
-import java.security.MessageDigest;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ParsecRsaSignature implements ParsecSignatureInfo {
     // keep this in order of priority
-    SHA512_WITH_RSA("SHA512withRSA",
-        pkcs1WithHash(PsaAlgorithm.Algorithm.Hash.SHA_512),
-        () -> MessageDigest.getInstance("SHA-512")),
+    SHA512_WITH_RSA("SHA512withRSA", pkcs1WithHash(PsaAlgorithm.Algorithm.Hash.SHA_512),
+            () -> MessageDigest.getInstance("SHA-512")),
 
-    SHA256_WITH_RSA("SHA256withRSA",
-        pkcs1WithHash(PsaAlgorithm.Algorithm.Hash.SHA_256),
-        () -> MessageDigest.getInstance("SHA-256")),
+    SHA256_WITH_RSA("SHA256withRSA", pkcs1WithHash(PsaAlgorithm.Algorithm.Hash.SHA_256),
+            () -> MessageDigest.getInstance("SHA-256")),
 
     SHA256_PRECALCULATED_WITH_RSA("SHA256PrecalculatedWithRSA",
-        pkcs1WithHash(PsaAlgorithm.Algorithm.Hash.SHA_256),
-        () -> MessageDigest.getInstance("None")),
-    ;
+            pkcs1WithHash(PsaAlgorithm.Algorithm.Hash.SHA_256),
+            () -> MessageDigest.getInstance("None")),;
 
-    @Getter private final String algorithmName;
-    @Getter private final AsymmetricSignature parsecAlgorithm;
-    @Getter private final MessageDigestFactory messageDigestFactory;
+    @Getter
+    private final String algorithmName;
+    @Getter
+    private final AsymmetricSignature parsecAlgorithm;
+    @Getter
+    private final MessageDigestFactory messageDigestFactory;
 
     private static AsymmetricSignature pkcs1WithHash(PsaAlgorithm.Algorithm.Hash hash) {
         return AsymmetricSignature.newBuilder()
-            .setRsaPkcs1V15Sign(AsymmetricSignature.RsaPkcs1v15Sign.newBuilder()
-                .setHashAlg(AsymmetricSignature.SignHash.newBuilder()
-                    .setSpecific(hash)
-                    .build())
-                .build())
-            .build();
+                .setRsaPkcs1V15Sign(AsymmetricSignature.RsaPkcs1v15Sign.newBuilder()
+                        .setHashAlg(
+                                AsymmetricSignature.SignHash.newBuilder().setSpecific(hash).build())
+                        .build())
+                .build();
     }
 
     private static AsymmetricSignature pkcs1() {
         return AsymmetricSignature.newBuilder()
-            .setRsaPkcs1V15Sign(AsymmetricSignature.RsaPkcs1v15Sign.newBuilder()
-                .build())
-            .build();
+                .setRsaPkcs1V15Sign(AsymmetricSignature.RsaPkcs1v15Sign.newBuilder().build())
+                .build();
     }
 
     @Override
     public ParsecSignature create(ParsecClientAccessor parsecClientAccessor) {
-        //AsymmetricSignature.newBuilder().setRsaPss(AsymmetricSignature.RsaPss.newBuilder()
+        // AsymmetricSignature.newBuilder().setRsaPss(AsymmetricSignature.RsaPss.newBuilder()
         return new ParsecSignature(this, parsecClientAccessor);
     }
 
