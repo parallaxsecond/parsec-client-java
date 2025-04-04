68d68491d4b8e8ce259e303ff80f63722af77dbb
Sam Spycher <samspycher@users.noreply.github.com>
2025-04-04 17:38:50 +0200

    fix: RSAPublicKey that works with http client
    Signed-off-by: Sam Spycher <samspycher@users.noreply.github.com>

--- parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecRsaPrivateKey.java
+++ parsec-jca-java/src/main/java/org/parallaxsecond/parsec/jce/provider/ParsecRsaPrivateKey.java
@@ -1,38 +1,95 @@
 package org.parallaxsecond.parsec.jce.provider;
 
+import java.math.BigInteger;
+import java.security.interfaces.RSAPrivateCrtKey;
+
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 
-import java.math.BigInteger;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-
-// FIXME verify if we can get rid off the RSAPrivateKey inheritance
-@Builder
+/**
+ * A private key implementation for RSA keys stored in a secure element through Parsec.
+ * 
+ * This class implements RSAPrivateCrtKey to provide all necessary key parameters to the TLS stack,
+ * while ensuring private key material remains secure in the secure element.
+ */
 @Value
-public class ParsecRsaPrivateKey implements RSAPrivateKey {
+public class ParsecRsaPrivateKey implements RSAPrivateCrtKey {
+    private static final long serialVersionUID = 1234567L;
+
     @NonNull
     String parsecName;
     @NonNull
     String algorithm;
     @NonNull
     String format;
-    // FIXME should get rid of this
-    RSAPublicKey rsaPublicKey;
+    @NonNull
+    BigInteger publicExponent;
+    @NonNull
+    BigInteger modulus;
+
+    @Builder
+    private ParsecRsaPrivateKey(String parsecName, String algorithm, String format,
+            BigInteger publicExponent, BigInteger modulus) {
+        this.parsecName = parsecName;
+        this.algorithm = algorithm;
+        this.format = format;
+        this.publicExponent = publicExponent;
+        this.modulus = modulus;
+    }
 
     @Override
-    public BigInteger getPrivateExponent() {
-        throw new IllegalStateException("cannot be called");
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    @Override
+    public String getFormat() {
+        return format;
     }
 
     @Override
     public byte[] getEncoded() {
-        throw new IllegalStateException("cannot be called");
+        return null; // Private key material cannot be exported from secure element
+    }
+
+    @Override
+    public BigInteger getPrivateExponent() {
+        return null; // Private key material cannot be exported from secure element
     }
 
     @Override
     public BigInteger getModulus() {
-        return rsaPublicKey.getModulus();
+        return modulus;
+    }
+
+    @Override
+    public BigInteger getPublicExponent() {
+        return publicExponent;
+    }
+
+    @Override
+    public BigInteger getPrimeP() {
+        return null; // Private key material cannot be exported from secure element
+    }
+
+    @Override
+    public BigInteger getPrimeQ() {
+        return null; // Private key material cannot be exported from secure element
+    }
+
+    @Override
+    public BigInteger getPrimeExponentP() {
+        return null; // Private key material cannot be exported from secure element
+    }
+
+    @Override
+    public BigInteger getPrimeExponentQ() {
+        return null; // Private key material cannot be exported from secure element
+    }
+
+    @Override
+    public BigInteger getCrtCoefficient() {
+        return null; // Private key material cannot be exported from secure element
     }
 }
